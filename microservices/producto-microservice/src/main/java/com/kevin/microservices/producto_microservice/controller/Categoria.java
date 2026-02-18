6367e3279c2b41048fa5067b17e4629f85f8e82b
Kevin <kfornier@gmail.com>
2026-02-18 13:24:28 -0300

    controladores

---
+++ microservices/producto-microservice/src/main/java/com/kevin/microservices/producto_microservice/controller/Categoria.java
@@ -0,0 +1,46 @@
+package com.kevin.microservices.producto_microservice.controller;
+
+import com.kevin.microservices.producto_microservice.dto.CategoriaDto;
+import com.kevin.microservices.producto_microservice.service.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categoria")
+public class Categoria {
+
+    @Autowired
+    private CategoriaService service;
+
+    @PostMapping("/crear")
+    public ResponseEntity<String> crear(@RequestBody CategoriaDto dto) {
+        service.crear(dto);
+        return ResponseEntity.ok("Categoria Creada");
+    }
+
+    @GetMapping("/list")
+    public List<CategoriaDto> lista() {
+        return service.lista();
+    }
+
+    @GetMapping("/list/{id}")
+    public CategoriaDto listaIdDto(@PathVariable long id) {
+        return service.listaId(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<String> actualizar(@PathVariable long id,
+                                             @RequestBody CategoriaDto dto) {
+        service.editar(id, dto);
+        return ResponseEntity.ok("Categoria Actualizada");
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> eliminar(@PathVariable long id) {
+        service.borrar(id);
+        return ResponseEntity.ok("Categoria Eliminada");
+    }
+}
---
+++ microservices/producto-microservice/src/main/java/com/kevin/microservices/producto_microservice/controller/Producto.java
@@ -0,0 +1,46 @@
+package com.kevin.microservices.producto_microservice.controller;
+
+import com.kevin.microservices.producto_microservice.dto.ProductoDto;
+import com.kevin.microservices.producto_microservice.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/producto")
+public class Producto {
+
+    @Autowired
+    private ProductoService service;
+
+    @PostMapping("/crear")
+    public ResponseEntity<String> crear(@RequestBody ProductoDto dto) {
+        service.crear(dto);
+        return ResponseEntity.ok("Producto Creado");
+    }
+
+    @GetMapping("/list")
+    public List<ProductoDto> lista() {
+        return service.lista();
+    }
+
+    @GetMapping("/list/{id}")
+    public ProductoDto listaIdDto(@PathVariable long id) {
+        return service.listaId(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<String> actualizar(@PathVariable long id,
+                                             @RequestBody ProductoDto dto) {
+        service.editar(id, dto);
+        return ResponseEntity.ok("Producto Actualizado");
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> eliminar(@PathVariable long id) {
+        service.borrar(id);
+        return ResponseEntity.ok("Producto Eliminado");
+    }
+}
