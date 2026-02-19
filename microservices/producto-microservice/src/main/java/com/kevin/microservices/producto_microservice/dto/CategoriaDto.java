38d1912076cfae28ba60560a52bb718347f8069d
Kevin <kfornier@gmail.com>
2026-02-19 17:27:58 -0300

    dto

--- microservices/producto-microservice/src/main/java/com/kevin/microservices/producto_microservice/dto/CategoriaDto.java
+++ microservices/producto-microservice/src/main/java/com/kevin/microservices/producto_microservice/dto/CategoriaDto.java
@@ -1,5 +1,6 @@
 package com.kevin.microservices.producto_microservice.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CategoriaDto {
     private Long id;
+    @NotNull(message = "El nombre de la categoria es obligatorio")
     private String nombre;
     private String descripcion;
 }
--- microservices/producto-microservice/src/main/java/com/kevin/microservices/producto_microservice/dto/ProductoDto.java
+++ microservices/producto-microservice/src/main/java/com/kevin/microservices/producto_microservice/dto/ProductoDto.java
@@ -1,5 +1,6 @@
 package com.kevin.microservices.producto_microservice.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,11 +10,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ProductoDto {
     private Long id;
+    @NotNull(message = "El nombre del producto no puede estar vacio")
     private String nombre;
     private String descripcion;
+    @NotNull(message = "El precio del producto no puede estar vacio")
     private Double precio;
     private Integer stock;
     private String imagenUrl;
+    @NotNull(message = "La categoria del producto es obligatoria")
     private Long categoria;
 
 }
