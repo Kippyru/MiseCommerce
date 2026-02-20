ee301159395132d7c670fd18ff6e1af7cbde5b7e
Kevin <kfornier@gmail.com>
2026-02-20 11:03:13 -0300

    producto stock default to 0

--- microservices/producto-microservice/src/main/java/com/kevin/microservices/producto_microservice/model/Producto.java
+++ microservices/producto-microservice/src/main/java/com/kevin/microservices/producto_microservice/model/Producto.java
@@ -1,41 +1,48 @@
 package com.kevin.microservices.producto_microservice.model;
 
-@lombok.Getter
-@lombok.Setter@jakarta.persistence.Entity
-@jakarta.persistence.Table(name = "producto")
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "producto")
 public class Producto {
-@jakarta.persistence.Id
-@jakarta.persistence.GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
-@jakarta.persistence.Column(name = "id", nullable = false)
-private java.lang.Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
 
-@jakarta.validation.constraints.Size(max = 150)
-@jakarta.validation.constraints.NotNull
-@jakarta.persistence.Column(name = "nombre", nullable = false, length = 150)
-private java.lang.String nombre;
+    @Size(max = 150)
+    @NotNull
+    @Column(name = "nombre", nullable = false, length = 150)
+    private String nombre;
 
-@jakarta.validation.constraints.Size(max = 255)
-@jakarta.persistence.Column(name = "descripcion")
-private java.lang.String descripcion;
+    @Size(max = 255)
+    @Column(name = "descripcion")
+    private String descripcion;
 
-@jakarta.validation.constraints.NotNull
-@jakarta.persistence.Column(name = "precio", nullable = false)
-private java.lang.Double precio;
+    @NotNull
+    @Column(name = "precio", nullable = false)
+    private Double precio;
 
-@jakarta.validation.constraints.NotNull
-@org.hibernate.annotations.ColumnDefault("0")
-@jakarta.persistence.Column(name = "stock", nullable = false)
-private java.lang.Integer stock;
+    @NotNull
+    @ColumnDefault("0")
+    @Column(name = "stock", nullable = false)
+    private Integer stock;
 
-@jakarta.validation.constraints.Size(max = 255)
-@jakarta.persistence.Column(name = "imagen_url")
-private java.lang.String imagenUrl;
+    @Size(max = 255)
+    @Column(name = "imagen_url")
+    private String imagenUrl;
 
-@jakarta.validation.constraints.NotNull
-@jakarta.persistence.ManyToOne(fetch = jakarta.persistence.FetchType.LAZY, optional = false)
-@jakarta.persistence.JoinColumn(name = "categoria_id", nullable = false)
-private com.kevin.microservices.producto_microservice.model.Categoria categoria;
-
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "categoria_id", nullable = false)
+    private Categoria categoria;
 
 
 }
