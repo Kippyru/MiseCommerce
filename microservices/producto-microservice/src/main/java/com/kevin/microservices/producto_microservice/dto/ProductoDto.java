34d1e087fe3b2e0a00d9f59428c3f044516f346e
Kevin <kfornier@gmail.com>
2026-02-20 15:46:26 -0300

    removed comments

--- microservices/producto-microservice/src/main/java/com/kevin/microservices/producto_microservice/dto/ProductoDto.java
+++ microservices/producto-microservice/src/main/java/com/kevin/microservices/producto_microservice/dto/ProductoDto.java
@@ -10,10 +10,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ProductoDto {
     private Long id;
-    @NotNull(message = "El nombre del producto no puede estar vacio")   //todos estos notnull no siven
-    private String nombre;                                              //por como estoy haciendo el servicio y el mapper
-    private String descripcion;                                         //pero bueno los dejo ahi
-    @NotNull(message = "El precio del producto no puede estar vacio")   //y no solo en este caso, en los demas dto tmb
+    @NotNull(message = "El nombre del producto no puede estar vacio")
+    private String nombre;
+    private String descripcion;
+    @NotNull(message = "El precio del producto no puede estar vacio")
     private Double precio;
     private Integer stock;
     private String imagenUrl;
