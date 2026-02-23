88d19f789c65f693598eca497dfaefcc835619e8
Kevin <kfornier@gmail.com>
2026-02-23 11:40:30 -0300

    fear(carrito): implement cart module

---
+++ microservices/carrito-microservice/src/test/java/com/kevin/microservices/carrito_microservice/CarritoMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevin.microservices.carrito_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarritoMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
