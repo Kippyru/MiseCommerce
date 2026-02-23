a467de34399538b95f9d5830634ae7b76418edd9
Kevin <kfornier@gmail.com>
2026-02-23 13:51:22 -0300

    feat(carrito): implement carrito and carrito_item entities with relationships

---
+++ microservices/carrito-microservice/src/main/java/com/kevin/microservices/carrito_microservice/model/Carrito.java
@@ -0,0 +1,32 @@
+package com.kevin.microservices.carrito_microservice.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "carrito")
+public class Carrito {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Size(max = 100)
+    @NotNull
+    @Column(name = "cliente_id", nullable = false, length = 100)
+    private String clienteId;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "created_at", insertable = false, updatable = false)
+    private Instant createdAt;
+
+
+}
---
+++ microservices/carrito-microservice/src/main/java/com/kevin/microservices/carrito_microservice/model/CarritoItem.java
@@ -0,0 +1,37 @@
+package com.kevin.microservices.carrito_microservice.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "carrito_item")
+public class CarritoItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "carrito_id", nullable = false)
+    private Carrito carrito;
+
+    @NotNull
+    @Column(name = "product_id", nullable = false)
+    private Long productId;
+
+    @NotNull
+    @ColumnDefault("1")
+    @Column(name = "cantidad", nullable = false)
+    private Integer cantidad;
+
+
+}
